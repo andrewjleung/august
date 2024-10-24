8227eafb075df8f280f30bf9914205aaaf551316
Andrew Leung <andrewleung104@gmail.com>
2024-10-23 23:33:28 -0500

    Start modelling and rendering guitar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,142 @@
-import Image from "next/image";
+type Tone = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+type NoteName =
+	| "A"
+	| "A#"
+	| "Bb"
+	| "B"
+	| "C"
+	| "C#"
+	| "Db"
+	| "D"
+	| "D#"
+	| "Eb"
+	| "E"
+	| "F"
+	| "F#"
+	| "Gb"
+	| "G"
+	| "G#"
+	| "Ab";
+type Note = { name: NoteName; octave: number };
+
+const NOTE_TO_TONE: Record<NoteName, Tone> = {
+	A: 1,
+	"A#": 2,
+	Bb: 2,
+	B: 3,
+	C: 4,
+	"C#": 5,
+	Db: 5,
+	D: 6,
+	"D#": 7,
+	Eb: 7,
+	E: 8,
+	F: 9,
+	"F#": 10,
+	Gb: 10,
+	G: 11,
+	"G#": 11,
+	Ab: 12,
+};
+
+type Guitar = {
+	strings: Note;
+	numFrets: number;
+	capo?: number;
+};
+
+type Mute = {
+	type: "mute";
+	string: number;
+};
+
+type Finger = {
+	type: "finger";
+	string: number;
+	fret: number;
+	variation: boolean;
+	finger?: 1 | 2 | 3 | 4;
+};
+
+type Barre = {
+	type: "barre";
+	start: Finger;
+	end: Finger;
+};
+
+type Fingering = Mute | Finger | Barre;
+
+type Chord = {
+	fingerings: Fingering[];
+};
+
+function ChordChart({
+	guitar,
+	chord,
+}: {
+	guitar: Guitar;
+	chord: Chord;
+}) {
+	return (
+		<>
+			<div className="relative">
+				<div className="flex flex-col w-fit m-12">
+					{Array(guitar.numFrets)
+						.fill(null)
+						.map((_, i) => {
+							return (
+								<>
+									<div key={`fret-finger-${i}`} className="flex flex-row gap-8">
+										{guitar.strings.map((string, stringPosition) => {
+											const fret = i + 1;
+
+											return (
+												<div
+													key={`string-${stringPosition}-fret-${fret}`}
+													className="bg-black w-1 h-16"
+												/>
+											);
+										})}
+									</div>
+									<div
+										key={`fret-${i}`}
+										className="w-full h-1 bg-neutral-500"
+									/>
+								</>
+							);
+						})}
+				</div>
+			</div>
+		</>
+	);
+}
+
+const GUITAR: Guitar = {
+	strings: [
+		{ note: "E", octave: 2 },
+		{ note: "A", octave: 2 },
+		{ note: "D", octave: 3 },
+		{ note: "G", octave: 3 },
+		{ note: "B", octave: 3 },
+		{ note: "E", octave: 4 },
+	],
+	numFrets: 22,
+};
+
+const E_CHORD: Chord = {
+	fingerings: [
+		{ type: "finger", string: 1, fret: 2, variation: false, finger: 2 },
+		{ type: "finger", string: 2, fret: 2, variation: false, finger: 3 },
+		{ type: "finger", string: 3, fret: 1, variation: false, finger: 1 },
+	],
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	return (
+		<div>
+			<main>
+				<ChordChart guitar={GUITAR} chord={E_CHORD} />
+			</main>
+		</div>
+	);
 }
